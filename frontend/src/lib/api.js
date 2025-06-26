06630129d0b1f632d3fdc5a5f6192a0d86ab1fb4
Yassine Aba <yassineaba.dev@gmail.com>
2025-06-26 23:10:34 +0100

    production ready app

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,4 +1,4 @@
-import { axiosInstance } from "./axios";
+import axiosInstance from "./axios";
 
 export const signup = async (signupData) => {
   const response = await axiosInstance.post("/auth/signup", signupData);
