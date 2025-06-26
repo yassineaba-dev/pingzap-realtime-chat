56f08212def354f8f092e4213223dabce30d91cc
Yassine Aba <yassineaba.dev@gmail.com>
2025-06-26 17:11:40 +0100

    production ready app

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5001/api";
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api` || "http://localhost:5001/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
